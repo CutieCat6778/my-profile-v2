77754a502fa8789256bac8d30ef696b24c1b1ee1
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-02-17 19:52:46 +0100

    added public

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,19 @@ export default function MyApp({ Component, pageProps }) {
   };
 
   useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+    
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
